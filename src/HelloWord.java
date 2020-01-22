8cfcf2e8e85e6aecb16e5eb3957fb66476364897
Edward Nuñez <nunezedward@uniovi.es>
2020-01-22 13:59:50 +0100

    Añadiendo linea Nuevo cambio en Instancia STS 1

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -3,5 +3,6 @@ public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo Git");
 		System.out.println("Primer cambio ");
+		System.out.println("Nuevo cambio Instancia 1");
 	}
 }
